0367597f0b291331bbe609c20de707c96fbd846c
nikodev99 <nikhe.niama99@outlook.fr>
2023-07-29 02:33:14 +0100

    feat: Adding components of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter } from 'react-router-dom'
+import { About, Contact, Experience, Feedbacks, Navbar, StarsCanvas, Tech, Hero, Works } from './components'
+
 import './App.css'
 
 const App = () => {
   return (
-    <div>
-      3D Developer portfolio
+    <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
     </div>
+    </BrowserRouter>
   )
 }
 
